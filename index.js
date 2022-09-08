72c2c8b1cc1efe720c5893a3e8bfa85254fd170f
adheelali <108449541+adheelali@users.noreply.github.com>
2022-09-08 14:43:03 +0500

    added movie not found img

--- index.js
+++ index.js
@@ -1,56 +1,61 @@
-const cardListEl = document.querySelector('.card-list')
-const buttonEl = document.querySelector('.button')
-const inputEl = document.querySelector('.input')
-const searchedEl = document.querySelector('.movie__searched--name')
+const cardListEl = document.querySelector(".card-list");
+const buttonEl = document.querySelector(".button");
+const inputEl = document.querySelector(".input");
+const searchedEl = document.querySelector(".movie__searched--name");
+const spinnerEl = document.querySelector(".fa-spinner");
+const movieNotFoundEl = document.querySelector(".not-found__img--wrapper");
 let executed = false;
 let inputSearch = false;
-let mainSearch = false
 
 function onSearchMovie(event) {
   const movieData = event.target.value;
-  searchedEl.innerHTML = movieData 
+  searchedEl.innerHTML = movieData;
   inputSearch = true;
   executed = true;
+  spinnerEl.classList += " display-block";
+  // movieNotFoundEl.classList.remove('display-block')
   search(movieData);
-} 
+}
 
 function search(movieData) {
   if (executed) {
     main(movieData);
   }
-  
-  executed = false
-  
+
+  executed = false;
+
+  // input properties
+
   if (!inputSearch) {
-    inputEl.classList += ' input-shake'
-    buttonEl.classList += ' input-shake'
+    inputEl.classList += " input-shake";
+    buttonEl.classList += " input-shake";
   }
-
   setTimeout(() => {
-    inputEl.classList.remove('input-shake')
-    buttonEl.classList.remove('input-shake')
+    inputEl.classList.remove("input-shake");
+    buttonEl.classList.remove("input-shake");
   }, 600);
-
   setTimeout(() => {
-    inputSearch = false
+    inputSearch = false;
   }, 700);
 }
 
 async function main(movieData) {
-    const data = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=c968a92&s=${movieData}`);
-    const jsonData = await data.json();
-    console.log(jsonData)
-    cardListEl.innerHTML = jsonData.Search.map((movie) => cardHTML(movie)).join('');
-    mainSearch = true
-}  
-
-if (mainSearch) {
-  main();
-  mainSearch = false
+  const data = await fetch(
+    `https://www.omdbapi.com/?i=tt3896198&apikey=c968a92&s=${movieData}`
+  );
+  const jsonData = await data.json();
+  console.log(jsonData);
+  spinnerEl.classList.remove("display-block");
+  cardListEl.innerHTML = jsonData.Search.map((movie) => cardHTML(movie)).join(
+    ""
+  );
+  if (jsonData.Response = "False") {
+    movieNotFoundEl.classList += " display-block";
+  }
 }
 
 function cardHTML(movie) {
-   return `
+  return `
    <div class="card">
     <figure class="card__img--wrapper">
      <img class="card__img" src="${movie.Poster}" />
@@ -60,5 +65,5 @@ function cardHTML(movie) {
       <h3 class="card__type">Type: ${movie.Type}</h2>
       <h3 >Year: ${movie.Year}</h3>
     </div>  
-   </div>  `
+   </div>  `;
 }
